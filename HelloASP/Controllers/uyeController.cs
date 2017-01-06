bf06d5366aea1e2aa07f7434f9caf6d122ba2a0a
Dafa Alfendo <alfendodafa@gmail.com>
2017-01-06 16:33:33 +0700

    Add project files.

---
+++ HelloASP/Controllers/uyeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloASP.Controllers
+{
+    public class uyeController : Controller
+    {
+        // GET: uye
+        public ActionResult uye()
+        {
+            return View();
+        }
+    }
+}
